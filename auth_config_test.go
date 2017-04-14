b7fc8da7ee8a0fcf7930e92055c4f53fe05f138b
David Malone <dmalone@pivotal.io>
2017-04-14 00:41:14 -0400

    added tests to validate proper loading of VCAP_SERVICES

--- auth_config_test.go
+++ auth_config_test.go
@@ -12,9 +12,33 @@ func TestNewAuthConfigWithoutEnvVarsOrVcapServicesFails(t *testing.T) {
 }
 
 func TestNewAuthConfigViaVcapServices(t *testing.T) {
+	os.Setenv("VCAP_APPLICATION", vcapApplicationPayload)
+	os.Setenv("VCAP_SERVICES", vcapServicesPayload)
+
+	if _, err := newAuthConfig(); err != nil {
+		t.Errorf("expected no error but got %v", err)
+	}
+
+	os.Clearenv()
 }
 
 func TestNewAuthConfigViaPartialVcapServices(t *testing.T) {
+	os.Setenv("VCAP_APPLICATION", vcapApplicationPayload)
+	os.Setenv("VCAP_SERVICES", vcapServicesPartialPayload)
+
+	_, err := newAuthConfig()
+
+	if err == nil {
+		t.Error("expected an error")
+	}
+
+	expectedError := "Failed to load auth0 service from either VCAP_SERVICES or from environment vars - missing [ClientID]"
+
+	if err.Error() != expectedError {
+		t.Errorf("expected error message %s but got %s", expectedError, err.Error())
+	}
+
+	os.Clearenv()
 }
 
 func TestNewAuthConfigViaEnvVars(t *testing.T) {
---
+++ payloads_test.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+const auth0ServicePayload = `{
+  "credentials":{
+    "clientid":"test-client-id",
+    "secret":"test-secret",
+    "domain":"test-domain",
+    "callback-url":"test-callback-url"
+  },
+  "syslog_drain_url": "",
+  "volume_mounts": [],
+  "label": "user-provided",
+  "name": "auth0-admin",
+  "tags": []
+}`
+
+const auth0ServicePartialPayload = `{
+  "credentials":{
+    "secret":"test-secret",
+    "domain":"test-domain",
+    "callback-url":"test-callback-url"
+  },
+  "syslog_drain_url": "",
+  "volume_mounts": [],
+  "label": "user-provided",
+  "name": "auth0-admin",
+  "tags": []
+}`
+
+var vcapApplicationPayload = `{
+  "application_name": "test-app",
+  "name": "test-app"
+}`
+
+var vcapServicesPayload = fmt.Sprintf(`{
+  "user-provided": [
+    %s
+  ]
+}`, auth0ServicePayload)
+
+var vcapServicesPartialPayload = fmt.Sprintf(`{
+  "user-provided": [
+    %s
+  ]
+}`, auth0ServicePartialPayload)
