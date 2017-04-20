f35e8f3fd1dedf0b923277f4eb15aae15af64003
David Malone <dmalone@pivotal.io>
2017-04-19 23:25:24 -0400

    [#143967051] apparent use of invalid method. Might be Go 1.8 specific, but not investigating at this time

--- pkg/cohesioned/auth/config_handler.go
+++ pkg/cohesioned/auth/config_handler.go
@@ -3,18 +3,14 @@ package auth
 import (
 	"fmt"
 	"net/http"
-	"net/url"
+	"strings"
 
 	"github.com/cohesion-education/admin-api/pkg/cohesioned/config"
 )
 
 func ConfigHandler(cfg *config.AuthConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		url, err := url.Parse(cfg.Domain)
-		if err != nil {
-			message := fmt.Sprintf("AUTH0_DOMAIN misconfigured %s %v", cfg.Domain, err)
-			w.Write([]byte(message))
-		}
+		domain := strings.TrimPrefix(cfg.Domain, "https://")
 
 		authVars := fmt.Sprintf(`
 			var AUTH0_CLIENT_ID='%s';
@@ -22,7 +18,7 @@ func ConfigHandler(cfg *config.AuthConfig) http.HandlerFunc {
 			var AUTH0_CALLBACK_URL='%s';
 			var LOGOUT_URL='%s'
 			var AUTH0_LOGOUT_URL=AUTH0_DOMAIN+'/v2/logout?returnTo='+encodeURI(LOGOUT_URL)+'&client_id='+AUTH0_CLIENT_ID;
-			`, cfg.ClientID, url.Hostname(), cfg.CallbackURL, cfg.LogoutURL)
+			`, cfg.ClientID, domain, cfg.CallbackURL, cfg.LogoutURL)
 
 		w.Header().Set("Content-Type", "text/javascript")
 		w.Write([]byte(authVars))
