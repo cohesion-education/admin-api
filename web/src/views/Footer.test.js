2639aa0b387a0960c8a8054c9661d1b047e88616
David Malone <dmalone@pivotal.io>
2017-07-11 12:37:38 -0400

    moving web files to their own project

--- web/src/views/Footer.test.js
+++
@@ -1,8 +0,0 @@
-import Footer from './Footer'
-import { shallow } from 'enzyme'
-
-it('renders without crashing', () => {
-  const wrapper = shallow(<Footer />)
-  expect(wrapper.type()).toEqual('footer')
-  expect(wrapper.hasClass('footer')).toBeTruthy()
-});
--- web/src/views/Testimonial.test.js
+++
@@ -1,8 +0,0 @@
-import Testimonial from './Testimonial'
-import { shallow } from 'enzyme'
-
-it('renders without crashing', () => {
-  const wrapper = shallow(<Testimonial />)
-  expect(wrapper.type()).toEqual('div')
-  expect(wrapper.hasClass('testimonial-box')).toBeTruthy()
-});
